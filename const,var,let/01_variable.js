2822ae9ac1d31349e0de47c90a2fdd4f31bd8359
RyzenBlueRepo <aryan01010raj@gmail.com>
2024-10-30 05:47:49 +0000

    constant and variables

---
+++ const,var,let/01_variable.js
@@ -0,0 +1,21 @@
+const accountId = 561651
+let accountEmail="gtat@gmail.com"
+var accountPassword= "5464646"
+accountcity="Lucknow"
+let accountState;
+// accountId= 5
+
+console.log(accountId);
+
+accountEmail="dog@gmail.com"
+accountPassword="255525"
+accountcity= "pune"
+
+console.table([accountId,accountEmail,accountPassword,accountcity,accountState]);
+
+/*
+prefer not to use Var 
+because of issue in block scope and functional scope
+*/
+
+
