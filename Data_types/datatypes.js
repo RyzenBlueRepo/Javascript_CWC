ff43fbde001f7a390592b94be6ee992fdef6fe8a
RyzenBlueRepo <aryan01010raj@gmail.com>
2024-10-30 07:16:59 +0000

    datatypes

---
+++ Data_types/datatypes.js
@@ -0,0 +1,47 @@
+let names="Aryan Raj"
+let age= 20
+let x= 15+5+"Aryan"
+let y= "Aryan"+15+5
+
+
+// console.log("Aryan Raj");
+// console.table([names,age,x,y ]);
+
+// String= ""
+// Using double quotes:
+let carName1 = "Volvo XC60";
+
+// Using single quotes:
+let carName2 = 'Volvo XC60';
+
+// Number=(range 2e53)
+// With decimals:
+let x1 = 34.00;
+
+// Without decimals:
+let x2 = 34;
+// Bigint= too big to be represented by a normal JavaScript Number.
+let xb = BigInt("123456789012345678901234567890");
+// Boolean =T/F
+// let xbo = 5;
+// let ybo = 5;
+// let zbo = 6;
+// (xbo == ybo)       // Returns true
+// (xbo == zbo)       // Returns false
+// Undefined=In JavaScript, a variable without a value, has the value undefined. The type is also undefined.
+let car;    // Value is undefined, type is undefined
+car = undefined;    // Value is undefined, type is undefined
+// Null
+// Symbol
+// Object
+
+// You can use the JavaScript typeof operator to find the type of a JavaScript variable.
+// The typeof operator returns the type of a variable or an expression:
+typeof 314            // Returns "number"
+typeof 3.14           // Returns "number"
+typeof ""             // Returns "string"
+typeof "John"         // Returns "string"
+typeof null          // Return 'object'
+typeof undefined     // Return 'Undefined'
+
+console.table([typeof 314,typeof "Raj",typeof null, typeof undefined]);
